fa2f394b11d1d007ab867d4ba8edb49c958cc96a
Orit Kozolin <orit@nachum.co.il>
2019-12-29 21:23:32 +0200

    add components - header, list, tile

---
+++ src/components/list/List.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+interface Props {}
+export const List: React.FC<Props> = () => {
+  return <div>List</div>;
+};
---
+++ src/components/tile/Tile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const Tile: React.FC<Props> = () => {
+  return <div>Tile</div>;
+};
