91be50db0e0cf13ab94e5433fa980747d97f5cb3
Orit Kozolin <orit@nachum.co.il>
2020-01-02 20:35:53 +0200

    display images from server after query

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./list.css";
+import { Tile } from "../tile/Tile";
 
 interface Props {
   shows: any;
@@ -9,7 +10,12 @@ export const List: React.FC<Props> = ({ shows }) => {
   return (
     <div className="list">
       {shows.map((item: any) => (
-        <div key={item.show.id}> {item.show.name}</div>
+        <div key={item.show.id}>
+          <Tile
+            name={item.show.name}
+            pic={item.show.image ? item.show.image.medium : ""}
+          ></Tile>
+        </div>
       ))}
     </div>
   );
